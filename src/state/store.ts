1b4b22e5065c057193e46d181275d1abe102134e
Ruben Swieringa <ruben.swieringa@gmail.com>
2019-08-09 15:26:53 +0200

    DRY up store

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,16 +2,14 @@ import { createStore, combineReducers } from 'redux';
 
 import { albumReducer, AlbumState } from './album';
 
-enum StateKeys {
-  albums = 'albums',
-}
-
 export interface State {
-  [StateKeys.albums]: AlbumState;
+  albums: AlbumState;
 }
 
-const reducers = combineReducers({
-  [StateKeys.albums]: albumReducer,
+type StateMap = { [key in keyof State]: any };
+
+const reducers = combineReducers<StateMap>({
+  albums: albumReducer,
 });
 
 export const store = createStore(reducers);
